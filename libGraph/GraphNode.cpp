35d6d0df41c079de53f02bcdf6315026d769d0a8
Scoobadood <scoobadood@me.com>
2018-02-16 06:51:09 +1100

    FEATURE: Introduced Graph class

---
+++ libGraph/GraphNode.cpp
@@ -0,0 +1,5 @@
+#include "GraphNode.h"
+
+GraphNode::GraphNode( const Eigen::Vector3f& point, const Eigen::Vector3f& normal ) {
+
+}
---
+++ libGraph/GraphNode.h
@@ -0,0 +1,29 @@
+#include <Eigen/Core>
+
+class GraphNode {
+public:
+	/**
+	 * Make a GraphNode given an existing point and normal
+	 * @param point The point
+	 * @param normak The normal (assumed to be a unit vector );
+	 */
+	GraphNode( const Eigen::Vector3f& point, const Eigen::Vector3f& normal );
+
+	/**
+	 * @return the normal for this node
+	 */
+	const Eigen::Vector3f& normal() const {return mNormal; }
+
+	/**
+	 * @return the point for this node
+	 */
+	const Eigen::Vector3f& point() const {return mPoint; };
+
+
+private:	
+	/** The point in space */
+	Eigen::Vector3f mPoint;
+
+	/** The normal to the point */
+	Eigen::Vector3f mNormal;
+};
