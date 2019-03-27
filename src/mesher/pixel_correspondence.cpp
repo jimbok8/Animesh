4b26aca187fcaade61c3c12aa7444a52a02dab2a
Dave Durbin <Dave.d@canva.com>
2019-03-27 19:36:19 +1100

    FT: Instrumented Correspondence code.

--- src/mesher/pixel_correspondence.cpp
+++ src/mesher/pixel_correspondence.cpp
@@ -30,6 +30,7 @@ compute_correspondences(const std::vector<std::string>& file_names,
 	size_t current_frame_idx = 0;
 	for(auto file_name : file_names) {
 		PgmData pgm = read_pgm(file_name);
+		// cout << "Processing " << file_name << endl;
 
 		size_t source_pixel_idx = 0;
 		size_t current_pixel_idx = 0;
@@ -38,48 +39,55 @@ compute_correspondences(const std::vector<std::string>& file_names,
 				int vertex = pgm.data.at(source_pixel_idx);
 				// Ignore background
 				if( vertex != 0 ) {
+					// cout << "V:" << vertex << "==> (" << current_frame_idx << ", " << current_pixel_idx << ")" << endl;
 					vertex_to_frame_pixel.insert( make_pair( vertex, make_pair(current_frame_idx, current_pixel_idx)));
 					current_pixel_idx++;
 				}
 				++source_pixel_idx;
 			}
 		}
-		cout << "corr: frame " << current_frame_idx << " has " << current_pixel_idx << " pixels" << endl;
+		// cout << "corr: frame " << current_frame_idx << " has " << current_pixel_idx << " pixels" << endl;
 		current_frame_idx++;
 	}
 
 	// Report on vertex to frame pixel
 	int num_frames = file_names.size();
 	int frame_pixel_count[num_frames];
-	for( int i=; i<num_frames; ++i) frame_pixel_count[i] = 0;
+	for( int i=0; i<num_frames; ++i) {
+		frame_pixel_count[i] = 0;
+	}
+
 	auto it = vertex_to_frame_pixel.begin();
 	int vertex = it->first;
+	int vertex_count = 0;
 	while( it != vertex_to_frame_pixel.end() ) {
 		if( it->first == vertex ) {
 			frame_pixel_count[it->second.first]++;
 			it++;
-
 		} else {
-
+			cout << "Vertex : " << vertex_count++ << "(" << vertex << ")" << ", Frame counts [";
+			for( int i=0; i<num_frames; ++i) {
+				cout << frame_pixel_count[i] << "  ";
+				frame_pixel_count[i] = 0;
+			}
+			cout << "]" << endl;
+			vertex = it->first;
 		}
 	}
-
-
-
-
 
 	// We now have a map from vertices to all corresponding frame/pixel pairs
 	// A correspondence is a vector of all frame/pixel pairs that have the same vertex
 	correspondences.clear();
+	vector<pair<unsigned int, unsigned int>> correspondence;
+	int corr = 0;
 	for (auto it = vertex_to_frame_pixel.begin(); it != vertex_to_frame_pixel.end(); ) {
-		vector<pair<unsigned int, unsigned int>> correspondence;
+		correspondence.clear();
 		unsigned int vertex_id = it->first;
 		do {
-			unsigned int frame_idx = it->second.first;
-			unsigned int pixel_idx = it->second.second;
-			correspondence.push_back(make_pair(frame_idx, pixel_idx));
+			correspondence.push_back(it->second);
 			++it;
 		} while( (it != vertex_to_frame_pixel.end()) && (vertex_id == it->first));
+		cout << "Correspondence " << corr++ << "("<<vertex_id<<") : " << correspondence.size() << endl;
 		correspondences.push_back(correspondence);
 	}
 }
